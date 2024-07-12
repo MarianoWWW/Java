f9abbf627d6bdd6233ea40bc7d6db98e5e548999
Mariano <marianoworoszylo@hotmail.com>
2024-07-12 18:17:09 -0300

    A ver

---
+++ Programa 2.2/src/Inicial_2_2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Inicial_2_2 {
+    public static void main(String[] args) throws Exception {
+        // 2.2. Se ingresan las notas de dos evaluaciones de un alumno. Determinar la nota promedio.
+
+        Scanner scanner = new Scanner(System.in);
+
+        int nota1, nota2;
+        double promedio;
+
+        do{
+            System.out.println("Ingresar la primera nota (numero entero)");
+            nota1 = scanner.nextInt();
+            if (nota1 <0 || nota1 >10){
+                System.out.println("Error. Esa nota no existe, intente nuevamente.");
+            }
+        }while (nota1 <0 || nota1 >10);
+
+        do{
+            System.out.println("Ingresar la segunda nota (numero entero)");
+            nota2 = scanner.nextInt();
+            if (nota2 <0 || nota2 >10){
+                System.out.println("Error. Esa nota no existe, intente nuevamente.");
+            }
+        }while (nota2 <0 || nota2 >10);
+
+        promedio = (nota1 + nota2)/2.0; // casteo con el 2.0
+
+        System.out.printf("El promedio es de : %.1f%n", promedio);
+
+        scanner.close();
+    }
+}
