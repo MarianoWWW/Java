e4e94506154216db90d3c1a889fdc471e5ffcc07
Mariano <marianoworoszylo@hotmail.com>
2024-07-17 16:01:34 -0300

    5.1.1

---
+++ Programa 5.1.1/src/Inicial_5_1_1.java
@@ -0,0 +1,9 @@
+public class Inicial_5_1_1 {
+    public static void main(String[] args) throws Exception {
+        
+        for (int i=100 ; i<=500 ; i = i+2) {
+            System.out.printf(" "+ (i));
+        }
+
+    }
+}
