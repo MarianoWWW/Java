09a485bc22f59029eb94b41fefa3559e7a4f507d
Mariano <marianoworoszylo@hotmail.com>
2024-07-18 13:37:23 -0300

    5.1.9

---
+++ Programa 5.1.9/src/Inicial_5_1_9.java
@@ -0,0 +1,9 @@
+public class Inicial_5_1_9 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\t\tImpares\t\tPares");
+
+        for (int i = 0 ; i < 30 ; i = i + 2) {
+                System.out.println("\t\t   "+ (i+1) + "\t\t  "+ (i));
+        }
+    }
+}
